49d6b3fc9d9a1d89a1ef0283601e3263fddfc994
semiharsan <40902660+semiharsan@users.noreply.github.com>
2023-07-28 15:39:03 +0100

    Update hello.py

--- app/hello.py
+++ app/hello.py
@@ -1,1 +1,10 @@
+from flask import Flask
 
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "Congratulations, You have just published a demo python application on AWS"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000, debug=True)
