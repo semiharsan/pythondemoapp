d18749e8132e375f6b1484a42022cf426b974372
semiharsan <40902660+semiharsan@users.noreply.github.com>
2023-07-29 20:29:36 +0100

    Update hello.py

--- app/hello.py
+++ app/hello.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Congratulations, You have just published a demo python application on AWS"
+    return "Congratulations !!! You just have published a demo python application on AWS ECS Service"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000, debug=True)
