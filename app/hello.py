9981ff4a137c979c75ba1ce0cd76a009c1895a69
semiharsan <40902660+semiharsan@users.noreply.github.com>
2023-08-01 10:07:43 +0100

    Update hello.py

--- app/hello.py
+++ app/hello.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Congratulations !!! You just have published a demo python application on AWS ECS Service. Keep Working To Update Your Knowledge"
+    return "Congratulations !!! You just have published python application to on-premise Kubernetes Cluster."
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000, debug=True)
