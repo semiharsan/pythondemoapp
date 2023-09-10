109fe589f7b882547112a8b62f00b316dc38fed0
semiharsan <40902660+semiharsan@users.noreply.github.com>
2023-09-10 13:26:39 +0100

    Update hello.py

--- app/hello.py
+++ app/hello.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Congratulations !!! You just have published python application to on-premise Kubernetes Cluster. Now go to next step to install nginx for certificate and external network access"
+    return "Congratulations !!! You just have published python application on Aws EKS cluster with Fargate Profile along with free ssl certificate..."
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000, debug=True)
